39adac75bbe60c64544e134184b236cacd59c356
jhiekkap <jari.hiekkapelto@gmail.com>
2022-07-01 21:51:36 +0300

    add label

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -9,7 +9,7 @@ const PokemonList = ({ pokemonList }) => {
           <div
             className="list-item-name"
           >
-            {name}
+          POKENAME:  {name}
           </div>
         </Link>
       ))}
